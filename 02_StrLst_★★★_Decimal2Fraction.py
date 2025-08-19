11583ba3f3e21af3ef07fe4b4e2382e134b5b852
aiTemUwU <6838266021@student.chula.ac.th>
2025-08-19 11:41:38 +0700

    Update 02_StrLst_★★★_Decimal2Fraction.py

--- 02_StrLst_★★★_Decimal2Fraction.py
+++ 02_StrLst_★★★_Decimal2Fraction.py
@@ -6,4 +6,4 @@ A = int(whole + nonRep + rep)-int(whole + nonRep)
 B = int("".join("9" for i in range(len(rep)))+"".join("0" for i in range(len(nonRep))))
 gcd = math.gcd(A, B)
 
-print(f"{int(A//gcd)} / {int(B/gcd)}")
+print(f"{int(A//gcd)} / {int(B//gcd)}")
